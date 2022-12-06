789b624c93649581c4328e44fa37922e1cdc2887
gedeon kakulia <115647528+Milltiades@users.noreply.github.com>
2022-12-06 19:58:44 +0400

    Add files via upload

---
+++ fylo-landing-page-with-two-column-layout-master/app.js
@@ -0,0 +1,18 @@
+let btn1 = document.getElementById('pH')
+let btn2main = document.getElementById('btn2main')
+
+function btnBtn(){
+    btn1.style.visibility='visible'
+    setTimeout(()=>{
+        btn1.style.visibility='hidden'
+    }, 3000)
+
+}
+
+
+function btn2Btn() {
+    btn2main.style.visibility = 'visible'
+    setTimeout(()=>{
+        btn2main.style.visibility='hidden'
+    }, 3000)
+}
